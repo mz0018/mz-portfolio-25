84d580c9993ca2a820ad6c60e37cdfef9bd8b4b6
mz0018 <martinezhanzmenzi@gmail.com>
2026-01-18 21:10:43 +0800

    React 19/ Tailwind initial state

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-red-500 text-2xl">Under Construction pelang daytuy parey!</p>
+    </>
+  )
+}
+
+export default App
